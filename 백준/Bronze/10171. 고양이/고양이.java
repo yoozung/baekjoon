43d2b5751c08c5e4906fc0561e010c257dc034f9
yoozung <83578464+yoozung@users.noreply.github.com>
2025-01-31 16:31:50 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] lgs) throws IOException {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+        }
+}
