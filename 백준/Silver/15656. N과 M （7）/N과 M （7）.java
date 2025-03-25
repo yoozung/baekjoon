dac0072292cc29ea984778b8101f6a3ffb7b29b8
yoozung <83578464+yoozung@users.noreply.github.com>
2025-03-25 17:34:34 +0900

    [Silver III] Title: N과 M (7), Time: 696 ms, Memory: 127268 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner; 
+
+public class Main {
+    static int n, m;
+    static int[] nums;              // 진수로 쓸 숫자들
+    static int[] printArr;          // 결과 저장용
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();  // 숫자 갯수
+        m = sc.nextInt();  // 진수
+
+        printArr = new int[m];
+        nums = new int[n];
+
+        for (int i = 0 ; i<n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        Arrays.sort(nums);
+        
+        recur(0);
+        System.out.println(sb.toString());
+    }
+
+    static void recur(int cur) {
+        if (cur == m) {
+            for (int num : printArr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) { 
+            printArr[cur] = nums[i];
+            recur(cur + 1 );
+        }
+    }
+
+}
